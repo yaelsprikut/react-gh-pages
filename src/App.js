3d17eb1427aa61a4f00a3be86f751893777f5d17
Yael Sprikut <yael@YaelMacPro.local>
2021-03-16 18:03:42 -0400

    nikita

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Yael's Wonderland</h1>
+          <h1 className="App-title">Welcome to Yael's Wonderland. Nikita Silvestrov is the best.</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
